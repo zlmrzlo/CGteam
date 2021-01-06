802673c5d5d1ebd69d9a001314d1bc7c4872c861
karzel <john199568@naver.com>
2021-01-06 17:04:51 +0900

    이윤주_튜토리얼_Crosshair
    Crosshair까지 만든 튜토리얼

---
+++ yoonjoo_tutorial/Practice2/Assets/Scripts/Gun.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public string gunName; // 총 이름
+    public float range; // 사거리
+    public float accuracy; // 정확도
+    public float fireRate; // 연사속도
+    public float reloadTime; // 재장전 속도
+
+    public int damage; // 데미지
+    public int reloadBulletCount; // 총알 재장전 개수
+    public int currentBulletCount; // 탄알집에 남은 총알 갯수
+    public int maxBulletCount; // 최대 소유 가능 개수
+    public int carryBulletCount; // 현재 소유 총알 개수
+
+    public float retroActionForce; // 반동 세기
+    public float retroActionFineSightForce; // 정조준시 반동 세기
+
+    public Vector3 fineSightOriginPos;
+    public Animator anim;
+    public ParticleSystem muzzleFlash;
+
+    public AudioClip fireSound;
+}
---
+++ yoonjoo_tutorial/Practice2/Assets/Scripts/HUD.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    // 필요 컴포넌트
+    [SerializeField]
+    private GunController theGunController;
+    private Gun currentGun;
+
+    // 필요시 HUD 호출
+    [SerializeField]
+    private GameObject go_BulletHUD;
+
+    // 총알 개수 반영
+    [SerializeField]
+    private Text[] text_Bullet;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckBullet();
+    }
+    private void CheckBullet()
+    {
+        currentGun = theGunController.GetGun();
+        text_Bullet[0].text = currentGun.carryBulletCount.ToString();
+        text_Bullet[1].text = currentGun.reloadBulletCount.ToString();
+        text_Bullet[2].text = currentGun.currentBulletCount.ToString();
+
+    }
+}
