81f511fe2fba38c6e7141962cc80c8e7683303dc
kirian <john199568@naver.com>
2021-04-11 16:06:51 +0900

    04.11 스테이지
    파일을 올립니다.

---
+++ Graduation2/Assets/download/Script/LightManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+    [SerializeField]
+    private Mesh[] meshes;
+    private MeshFilter mesh;
+    public bool lightOn;
+    public bool isOpen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightOn = false;
+        isOpen = false;
+        mesh = GetComponent<MeshFilter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isOpen)
+        {
+            changeLight();
+        }
+        else
+        {
+            mesh.sharedMesh = meshes[1];
+
+        }
+    }
+    private void changeLight()
+    {
+        if(lightOn)
+        {
+            mesh.sharedMesh = meshes[1];
+        }
+        else
+        {
+            mesh.sharedMesh = meshes[0];
+        }
+    }
+}
---
+++ Graduation2/Assets/download/Script/SwitchManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SwitchManager : MonoBehaviour
+{
+    public bool turnOn;
+    public bool isOpen;
+    [SerializeField]
+    protected LightManager light;
+    [SerializeField]
+    protected float turnOnTime;
+
+ 
+    // Start is called before the first frame update
+
+    protected abstract void turnOnSwitch();
+    protected abstract IEnumerator SwitchCoroutine();
+}
