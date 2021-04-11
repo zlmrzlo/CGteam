57bcb93d51113e2e0049aafcbd5ed7c324203e2d
kirian <john199568@naver.com>
2021-04-11 17:20:09 +0900

    04.11 스테이지
    재 업로드

---
+++ Graduation2/Assets/download/Script/DoorManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    [SerializeField]
+    private SwitchManager[] switchs;
+    [SerializeField]
+    private GameObject door;
+    [SerializeField]
+    private float doorSpeed;
+    private Vector3 doorFinishPos;
+
+    private bool openDoor = false;
+    private int turnOnSwitch;
+
+    void Start()
+    {
+        turnOnSwitch = 0;
+        doorFinishPos = new Vector3(door.transform.position.x, door.transform.position.y - 3.0f, door.transform.position.z);
+    }
+
+    void Update()
+    {
+        CheckSwitch();
+    }
+
+    private void CheckSwitch()
+    {
+        for(int i = 0; i<switchs.Length; i++)
+        {
+            if (switchs[i].turnOn == true)
+            {
+                turnOnSwitch++;
+            }
+        }
+        if(turnOnSwitch == switchs.Length)
+        {
+            OpenDoor();
+        }
+        else
+        {
+            turnOnSwitch = 0;
+        }
+    }
+    private void OpenDoor()
+    {
+        for (int i = 0; i < switchs.Length; i++)
+        {
+            switchs[i].isOpen = true;
+        }
+        door.transform.position = Vector3.MoveTowards(door.transform.position, doorFinishPos, doorSpeed);
+
+    }
+}
---
+++ Graduation2/Assets/download/Script/GravityController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GravityController : MonoBehaviour
+{
+    [SerializeField]
+    private gravityDirection changeTo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        var obj = other.gameObject;
+        obj.GetComponent<Object>().changeGravity(changeTo);
+    }
+}
