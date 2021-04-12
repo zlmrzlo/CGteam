965321dadf6dac111df677ef0b1afd3ad417d75f
zlmrzlo <zlmrzlo@naver.com>
2021-04-12 21:25:36 +0900

    Upload

---
+++ Assets/Scripts/Player Scripts/GravityEffect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityEffect : MonoBehaviour
+{
+    private void OnTriggerEnter(UnityEngine.Collider other)
+    {
+        if(other.transform.tag == "Player")
+        {
+            print("Player");
+        }
+    }
+}
