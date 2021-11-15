2b3625ad8893b862dd6cfdcb43f15a12b54e661f
zlmrzlo <zlmrzlo@naver.com>
2021-11-15 16:45:39 +0900

    teleport Complete

---
+++ Game/Assets/Scripts/Enemy AI/LavaBulletDestroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaBulletDestroy : MonoBehaviour
+{
+    public GameObject gameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
